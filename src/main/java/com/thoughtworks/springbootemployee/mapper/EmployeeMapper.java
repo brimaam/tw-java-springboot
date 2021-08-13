d834fd4fcca0f9f61691cb4f06477e8d3db79840
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-13 14:54:09 +0800

    [employee][AmeerBrima] feat: added EmployeeResponse and toResponse method in Mapper  method

--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.model.EmployeeRequest;
+import com.thoughtworks.springbootemployee.model.EmployeeResponse;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -13,4 +14,13 @@ public class EmployeeMapper {
 
         return employee;
     }
+
+    public EmployeeResponse toResponse(Employee employee) {
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        BeanUtils.copyProperties(employee, employeeResponse);
+
+        return employeeResponse;
+    }
+
+
 }
---
+++ src/main/java/com/thoughtworks/springbootemployee/model/EmployeeResponse.java
@@ -0,0 +1,50 @@
+package com.thoughtworks.springbootemployee.model;
+
+
+public class EmployeeResponse {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
